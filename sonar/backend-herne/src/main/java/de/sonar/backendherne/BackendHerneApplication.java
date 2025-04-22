d164b63d4bfed35b3763bd73b0b611398521465c
fab38114 <fabian.serves@iml.fraunhofer.de>
2025-04-22 10:07:32 +0200

    Add empty project for backend herne

---
+++ sonar/backend-herne/src/main/java/de/sonar/backendherne/BackendHerneApplication.java
@@ -0,0 +1,13 @@
+package de.sonar.backendherne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHerneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHerneApplication.class, args);
+	}
+
+}
---
+++ sonar/backend-herne/src/test/java/de/sonar/backendherne/BackendHerneApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sonar.backendherne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHerneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
