d514684be3aa02c379b3a29c99ed7d2aa00fd35c
Till <tillphilipp.patron@gmail.com>
2025-04-22 10:29:21 +0200

    Klassennamen an Dateinamen angepasst

--- sonar/herne-backend/src/main/java/de/sonar/hernebackend/HerneBackendApplication.java
+++ sonar/herne-backend/src/main/java/de/sonar/hernebackend/HerneBackendApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendHerneApplication {
+public class HerneBackendApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Herne Backend");
-		SpringApplication.run(BackendHerneApplication.class, args);
+		SpringApplication.run(HerneBackendApplication.class, args);
 	}
 
 }
