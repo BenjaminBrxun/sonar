161b97d20fd4f39e07ab6abd66e599aea54471ae
Till <tillphilipp.patron@gmail.com>
2025-04-22 10:24:40 +0200

    Refactoring und Anpassung der GitHub Actions

--- sonar/herne-backend/src/main/java/de/sonar/hernebackend/HerneBackendApplication.java
+++ sonar/herne-backend/src/main/java/de/sonar/hernebackend/HerneBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendHerneApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Herne Backend");
 		SpringApplication.run(BackendHerneApplication.class, args);
 	}
 
