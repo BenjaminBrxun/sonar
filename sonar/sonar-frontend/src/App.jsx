91d2a31b689f985272dbf9ed408be2569356e834
Till <tillphilipp.patron@gmail.com>
2025-05-21 14:56:54 +0200

    Erster Ansatz für Event Cards
    Hinweis: Cards sind React eigene Components

--- sonar/sonar-frontend/src/App.jsx
+++ sonar/sonar-frontend/src/App.jsx
@@ -1,33 +1,25 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './App.scss'
 import {InterfaceHeader} from './components/interface/interfaceHeader/InterfaceHeader.jsx'
 import {InterfaceFooter} from './components/interface/interfaceFooter/InterfaceFooter.jsx'
 import {InteractiveMap} from "./components/interactivemap/InteractiveMap.jsx";
+import {SonarEvent} from "./model/sonarevent/SonarEvent.jsx";
 
 
 function App() {
-    const [page, setPage] = useState("map")
-
-    const [dataFromChild, setDataFromChild] = useState(false);
-
-    function handleDataFromChild(data) {
-        setDataFromChild(data);
-        setPage("map");
-        setDataFromChild(false);
-    }
-
-    function _toggle(newPage) {
-        if (page === newPage || dataFromChild) {
-            setPage("map");
-            setDataFromChild(false);
-        } else {
-            setPage(newPage)
-        }
-    }
 
     return (
-        <div className="sonar-body" style={{width:'100vw',height:'100vh'}}>
+        <div className="sonar-body">
             <InterfaceHeader/>
+            <div className="card-container">
+                <SonarEvent/>
+                <SonarEvent/>
+                <SonarEvent/>
+                <SonarEvent/>
+                <SonarEvent/>
+                <SonarEvent/>
+
+            </div>
             <InteractiveMap/>
             <InterfaceFooter/>
         </div>
