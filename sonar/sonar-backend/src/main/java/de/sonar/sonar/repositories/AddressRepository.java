9d2d29795934610cc2ccea3849d21808e4e49d53
nameless-interactive <147716431+nameless-interactive@users.noreply.github.com>
2025-05-27 12:07:14 +0200

    unnötige Dateien entfernt

--- sonar/sonar-backend/src/main/java/de/sonar/sonar/repositories/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package de.sonar.sonar.repositories;
-
-import de.sonar.sonar.datenmodell.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
