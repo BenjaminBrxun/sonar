922b8c3fe8f2675a80a7a4ef7864fcd6b7849dde
Nilzzz <nils@lol.de>
2025-06-19 12:51:50 +0200

    push

--- sonar/sonar-frontend/src/components/interactivemap/InteractiveMap.jsx
+++ sonar/sonar-frontend/src/components/interactivemap/InteractiveMap.jsx
@@ -5,13 +5,11 @@ import "./InteractiveMap.scss"
 export const InteractiveMap = () => {
     return (
         <div className="interactiveMap-container">
-            <div className="image-wrapper">
-                <img src={map} alt={"Interactive Map"} className="sonar-map_image" useMap="#map" width="400" height="379"/>
-                <map name="map">
-                    <area shape="circle" coords="337,300,44" alt="Computer" href="coffe.htm"/>
-                </map>
-                <div className="circle pulse green" style={{top: "300px", left: "337px"}}/>
-            </div>
+            <img src={map} alt={"Interactive Map"} className="sonar-map_image" useMap="#map"/>
+            <map name="map">
+                <area shape="circle" coords="215,495,15" alt="Computer" href="https://klpt.de/cdn/shop/files/Klappt._Logo_1500x1500_Zeichenflaeche_1.png?height=628&pad_color=f5f7f6&v=1659013043&width=1200"/>
+            </map>
+            <div className="circle pulse green c1"/>
         </div>
     );
 };
