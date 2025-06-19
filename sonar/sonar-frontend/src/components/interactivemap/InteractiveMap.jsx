a46d11517262d5ab1fb9c9de03a9a1fcdc9dcd9e
Max van Lier <vanlier.max@gmail.com>
2025-06-19 12:02:46 +0200

    FIX: map

--- sonar/sonar-frontend/src/components/interactivemap/InteractiveMap.jsx
+++ sonar/sonar-frontend/src/components/interactivemap/InteractiveMap.jsx
@@ -5,10 +5,13 @@ import "./InteractiveMap.scss"
 export const InteractiveMap = () => {
     return (
         <div className="interactiveMap-container">
-            <img src={map} alt={"Interactive Map"} className="sonar-map_image" useMap="#workmap"/>
-            <map name="workmap">
-                <area shape="circle" coords="200,200,50" alt="Computer" href="localhost:8080"/>
-            </map>
+            <div className="image-wrapper">
+                <img src={map} alt={"Interactive Map"} className="sonar-map_image" useMap="#map" width="400" height="379"/>
+                <map name="map">
+                    <area shape="circle" coords="337,300,44" alt="Computer" href="coffe.htm"/>
+                </map>
+                <div className="circle pulse green" style={{top: "300px", left: "337px"}}/>
+            </div>
         </div>
-    )
-}
+    );
+};
