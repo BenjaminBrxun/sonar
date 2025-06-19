088f8037679308ee77d953629568aedc797a9daf
Fabian Serves <fabian.serves@iml.fraunhofer.de>
2025-06-19 13:36:38 +0200

    Remove unused `EventStateHandler` and redundant `@RequiredArgsConstructor` in `ApprovedEventState`

--- sonar/sonar-backend/src/main/java/de/sonar/sonar/model/state/ApprovedEventState.java
+++ sonar/sonar-backend/src/main/java/de/sonar/sonar/model/state/ApprovedEventState.java
@@ -3,7 +3,6 @@ package de.sonar.sonar.model.state;
 import de.sonar.sonar.model.Event;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 public class ApprovedEventState implements EventState {
 
     @Override
