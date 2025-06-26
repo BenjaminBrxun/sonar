b65ba108fe928f17b443bcb607d8fc637d135f95
N5809 <73257371+N5809@users.noreply.github.com>
2025-06-26 12:22:09 +0200

    Z-index angepasst

--- sonar/sonar-frontend/src/components/interactivemap/InteractiveMap.jsx
+++ sonar/sonar-frontend/src/components/interactivemap/InteractiveMap.jsx
@@ -6,11 +6,11 @@ export const InteractiveMap = ({ isVisible = true}) => {
     return (
         <div className={`interactiveMap-container ${isVisible ? "visible" : "hidden"}`}>
             <img src={map} alt={"Interactive Map"} className="sonar-map_image"/>
-            <div className="circle c1 green pulse" title="Ort A" onClick={() => alert('Ort A!')}></div>
-            <div className="circle c2 green pulse" title="Ort B" onClick={() => alert('Ort B!')}></div>
-            <div className="circle c3 green pulse" title="Ort C" onClick={() => alert('Ort C!')}></div>
-            <div className="circle c4 green pulse" title="Ort D" onClick={() => alert('Ort D!')}></div>
-            <div className="circle c5 green pulse" title="Ort E" onClick={() => alert('Ort E!')}></div>
+            <div className="circle c1" title="Ort A" onClick={() => alert('Ort A!')}></div>
+            <div className="circle c2" title="Ort B" onClick={() => alert('Ort B!')}></div>
+            <div className="circle c3" title="Ort C" onClick={() => alert('Ort C!')}></div>
+            <div className="circle c4" title="Ort D" onClick={() => alert('Ort D!')}></div>
+            <div className="circle c5" title="Ort E" onClick={() => alert('Ort E!')}></div>
         </div>
     );
 };
