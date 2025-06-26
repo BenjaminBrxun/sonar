47734613587e489197ee2be9e86f1465da7324ab
Fabian Serves <fabian.serves@iml.fraunhofer.de>
2025-06-26 13:32:17 +0200

    Call it a day

--- sonar/herne-backend/src/main/java/de/sonar/hernebackend/controllers/EventController.java
+++ sonar/herne-backend/src/main/java/de/sonar/hernebackend/controllers/EventController.java
@@ -2,6 +2,7 @@ package de.sonar.hernebackend.controllers;
 
 import de.sonar.hernebackend.model.entity.Event;
 import de.sonar.hernebackend.services.*;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,11 +17,34 @@ public class EventController {
     private final EventDeclineService eventDeclineService;
     private final EventDeletionService eventDeletionService;
 
+
+    @Operation(
+            summary = "Register a new event.",
+            description = """
+                    Register a new event over MQTT in the sonar backend.
+                    
+                    If the event is already present in database or has an invalid state,
+                    an exception is thrown.
+                    """
+    )
     @PostMapping
     public Event registerNewEvent(@RequestBody Event newEvent) {
         return eventRegistrationService.registerNewEvent(newEvent);
     }
 
+    @Operation(
+            summary = "Proceed the event lifecycle.",
+            description = """
+                    Proceed the event lifecycle for a given event.
+                    
+                    The event have to be already registered in database,
+                    otherwise an exception is thrown.
+                    
+                    Valid state transitions:
+                    
+                
+                    """
+    )
     @PutMapping
     public Event proceedEvent(@RequestBody Event event) {
         return eventProcessionService.proceedEvent(event);
