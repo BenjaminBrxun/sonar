cb760b896b6ab6523ad6f77777a0aefa5f0bfbb1
N5809 <73257371+N5809@users.noreply.github.com>
2025-07-01 12:41:56 +0200

    background hinzugefügt und eingebunden

--- sonar/sonar-frontend/src/pages/+Layout.jsx
+++ sonar/sonar-frontend/src/pages/+Layout.jsx
@@ -1,6 +1,6 @@
 import {InterfaceHeader} from "../components/interface/interfaceHeader/InterfaceHeader.jsx";
-import {InteractiveMap} from "../components/interactivemap/InteractiveMap.jsx";
 import {InterfaceFooter} from "../components/interface/interfaceFooter/InterfaceFooter.jsx";
+import {Background} from "../components/background/Background.jsx";
 import React from "react";
 import {usePageContext} from "vike-react/usePageContext";
 import "./Layout.scss";
@@ -12,9 +12,11 @@ function Layout({ children }) {
     const pageContext = usePageContext();
     const isFilterPage = pageContext.urlPathname.startsWith("/filter");
     const isSearchPage = pageContext.urlPathname.startsWith("/search");
+    const isMapPage = pageContext.urlPathname.startsWith("/interactivemap");
 
     return (
         <div className="layout-wrapper">
+            {!isMapPage && <Background  />}
             <div className="ui">
                 {!isFilterPage && !isSearchPage && <InterfaceHeader />}
                 <div className="current-component">{children}</div>
