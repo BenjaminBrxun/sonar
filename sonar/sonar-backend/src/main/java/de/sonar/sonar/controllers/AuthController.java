7cd7b7f08b5e554bdcae7f0daa9363cf71edd001
Till <tillphilipp.patron@gmail.com>
2025-07-06 16:37:51 +0200

    Exception Handling hinzugefügt

--- sonar/sonar-backend/src/main/java/de/sonar/sonar/controllers/AuthController.java
+++ sonar/sonar-backend/src/main/java/de/sonar/sonar/controllers/AuthController.java
@@ -1,14 +1,20 @@
 package de.sonar.sonar.controllers;
-import de.sonar.sonar.dto.*;
+
+import de.sonar.sonar.dto.JwtResponse;
+import de.sonar.sonar.dto.LoginRequest;
+import de.sonar.sonar.dto.RegisterRequest;
 import de.sonar.sonar.model.User;
 import de.sonar.sonar.repositories.UserRepository;
 import de.sonar.sonar.security.JwtUtils;
-import lombok.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -35,12 +41,12 @@ public class AuthController {
             SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
             Date parsedBirthDate = formatter.parse(request.getBirthDate());
             user = User.builder().email(request.getEmail()).password(passwordEncoder.encode(request.getPassword())).username(request.getUsername()).birthDate(parsedBirthDate).build();
-            if(user == null) {
+            if (user == null) {
                 return ResponseEntity.badRequest().body("E-Mail existiert bereits");
             }
             userRepo.save(user);
-        } catch(ParseException e) {
-            //TODO Exception Handling
+        } catch (ParseException e) {
+            return ResponseEntity.status(500).body("Fehler be der Registrierung: " + e.getMessage());
         }
 
         return ResponseEntity.ok("Registrierung erfolgreich");
@@ -50,10 +56,10 @@ public class AuthController {
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
         log.info(request.toString());
         User user = userRepo.findByEmail(request.getEmail());
-        if(user == null) {
+        if (user == null) {
             throw new RuntimeException("User nicht gefunden");
         }
-        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falsches Passwort");
         }
 
