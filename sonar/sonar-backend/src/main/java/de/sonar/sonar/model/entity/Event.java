b3a360f2e56532eed4dbf1027a2f298b32471d47
nameless-interactive <147716431+nameless-interactive@users.noreply.github.com>
2025-07-10 12:16:18 +0200

    Builder-Pattern implementiert

--- sonar/sonar-backend/src/main/java/de/sonar/sonar/model/entity/Event.java
+++ sonar/sonar-backend/src/main/java/de/sonar/sonar/model/entity/Event.java
@@ -16,6 +16,22 @@ import java.util.List;
 @Builder
 public class Event {
 
+    public Event(Builder builder) {
+        this.name = builder.name;
+        this.description = builder.description;
+        this.headline = builder.headline;
+        this.address = builder.address;
+        this.categories = builder.categories;
+        this.startDate = builder.startDate;
+        this.endDate = builder.endDate;
+        this.applicant = builder.applicant;
+        this.processor = builder.processor;
+        this.deletedStateAt = builder.deletedStateAt;
+        this.price = builder.price;
+        this.minAge = builder.minAge;
+        this.restricted = builder.restricted;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
@@ -58,6 +74,92 @@ public class Event {
 
     public boolean isNew() {
         return this.id <= 0;
+    }
+
+    // Referenzimplementierung für das Builder-Pattern
+    public static class Builder {
+        private String name;
+        private String description;
+        private String headline;
+        private Address address;
+        private List<Category> categories;
+        private OffsetDateTime startDate;
+        private OffsetDateTime endDate;
+        private OffsetDateTime deletedStateAt;
+        private Organizer applicant;
+        private AdministrativeUser processor;
+        private float price;
+        private int minAge;
+        private boolean restricted;
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder headline(String headline) {
+            this.headline = headline;
+            return this;
+        }
+
+        public Builder address(Address address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder categories(List<Category> categories) {
+            this.categories = categories;
+            return this;
+        }
+
+        public Builder startDate(OffsetDateTime startDate) {
+            this.startDate = startDate;
+            return this;
+        }
+
+        public Builder endDate(OffsetDateTime endDate) {
+            this.endDate = endDate;
+            return this;
+        }
+
+        public Builder deletedStateAt(OffsetDateTime deletedStateAt) {
+            this.deletedStateAt = deletedStateAt;
+            return this;
+        }
+
+        public Builder applicant(Organizer applicant) {
+            this.applicant = applicant;
+            return this;
+        }
+
+        public Builder processor(AdministrativeUser processor) {
+            this.processor = processor;
+            return this;
+        }
+
+        public Builder price(float price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder minAge(int minAge) {
+            this.minAge = minAge;
+            return this;
+        }
+
+        public Builder restricted(boolean restricted) {
+            this.restricted = restricted;
+            return this;
+        }
+
+        public Event build() {
+            return new Event(this);
+        }
     }
 
 }
